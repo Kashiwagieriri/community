706bf6b095f5758321df8bf1c689c95250835347
Kashiwagieriri <s7106249583@gmail.com>
2021-11-06 23:21:42 +0800

    add GitProvider AuthorizeController  dto

---
+++ README.md
@@ -0,0 +1,7 @@
+[bootstrap]https://v3.bootcss.com/components/#navbar
+[bootstrap]https://v3.bootcss.com/getting-started/
+[spring]https://spring.io/guides
+[spring]https://start.spring.io/
+[oauth]https://docs.github.com/en/developers/apps/building-oauth-apps
+[oauthsetting]https://github.com/settings/applications/1759901
+[oauthguide]https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.controller.provider.GithubProvider;
+import com.example.demo.dto.AccessTokenDTO;
+import com.example.demo.dto.GithubUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("b762375f695489fa1e28");
+        accessTokenDTO.setClient_secret("78736fa84bebf4f749222317415fca3b4608b310");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
