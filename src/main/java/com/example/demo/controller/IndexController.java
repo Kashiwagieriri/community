79b9981db48c52956ccfa85ae0287f3c7f6e1180
Kashiwagieriri <s7106249583@gmail.com>
2021-11-07 22:04:07 +0800

    add publish

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -20,14 +20,17 @@ public class IndexController {
     @GetMapping("/")
     public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies){
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
+        for (Cookie cookie : cookies) {
+            if (cookies != null) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
+                //获取cookie中的token，并且和数据中的token对比，获取用户信息不为空时，再次设置session
             }
         }
         return "index";
