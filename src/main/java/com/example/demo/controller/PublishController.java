88f4ab1f87df7477cd0021fff99bd8b7e8af6539
Kashiwagieriri <s7106249583@gmail.com>
2021-11-08 13:26:09 +0800

    complete publishpage

--- src/main/java/com/example/demo/controller/PublishController.java
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -1,12 +1,86 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.QuestionMapper;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
      return "publish";
     }
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            //接受内容
+            HttpServletRequest request,
+            Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        //回显
+        if(title == null || title == ""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if(description == null || description == ""){
+            model.addAttribute("error","内容不能为空");
+            return "publish";
+        }
+        if(tag == null || tag == ""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+        //为空提示
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        //通过token获取user信息，绑定到session
+        if(user == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        //user不存在，信息返回前端
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtModified(System.currentTimeMillis());
+        question.setGmtCreate(question.getGmtCreate());
+        questionMapper.create(question);
+        //构建question对象
+        return "redirect:/";
+    }
 }
+
