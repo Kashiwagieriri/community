5f2306d3119c7041ab816ec85f14795a61a43615
Kashiwagieriri <s7106249583@gmail.com>
2021-11-10 18:35:11 +0800

    add pagination

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PaginationDTO;
 import com.example.demo.dto.QuestionDTO;
 import com.example.demo.mapper.QuestionMapper;
 import com.example.demo.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +29,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1" ) Integer page,
+                        @RequestParam(name = "size",defaultValue = "5" ) Integer size
+    ){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length !=0)
             for (Cookie cookie : cookies) {
@@ -41,8 +46,8 @@ public class IndexController {
                 }
                 //获取cookie中的token，并且和数据中的token对比，获取用户信息不为空时，再次设置session
             }
-            List<QuestionDTO> questionList = questionService.list();
-            model.addAttribute("questions",questionList);
+            PaginationDTO pagination = questionService.list(page,size);
+            model.addAttribute("pagination",pagination);
         return "index";
     }
 }
