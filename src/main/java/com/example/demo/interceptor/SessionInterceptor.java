5677a446229f8d27a9e39e3d96f24b2d6adef360
Kashiwagieriri <s7106249583@gmail.com>
2021-12-21 20:44:37 +0800

    fixbug

--- src/main/java/com/example/demo/interceptor/SessionInterceptor.java
+++ src/main/java/com/example/demo/interceptor/SessionInterceptor.java
@@ -1,66 +1,58 @@
-package com.example.demo.interceptor;
+    package com.example.demo.interceptor;
 
-import com.example.demo.mapper.UserMapper;
-import com.example.demo.model.User;
-import com.example.demo.model.UserExample;
-import com.example.demo.service.NotificationService;
-import com.sun.istack.Nullable;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+    import com.example.demo.mapper.UserMapper;
+    import com.example.demo.model.User;
+    import com.example.demo.model.UserExample;
+    import com.example.demo.service.NotificationService;
+    import com.sun.istack.Nullable;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import org.springframework.web.servlet.HandlerInterceptor;
+    import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.List;
+    import javax.servlet.http.Cookie;
+    import javax.servlet.http.HttpServletRequest;
+    import javax.servlet.http.HttpServletResponse;
+    import java.util.List;
 
-@Service
-public class SessionInterceptor implements HandlerInterceptor {
+    @Service
+    public class SessionInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private NotificationService notificationService;
-    @Value("${github.redirect.uri}")
-    private String githubRedirectUri;
+        @Autowired
+        private UserMapper userMapper;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //设置 context 级别的属性
-        request.getServletContext().setAttribute("githubRedirectUri", githubRedirectUri);
-        // 没有登录的时候也可以查看导航
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token") && StringUtils.isNotBlank(cookie.getValue())) {
-                    String token = cookie.getValue();
-                    UserExample userExample = new UserExample();
-                    userExample.createCriteria()
-                            .andTokenEqualTo(token);
-                    List<User> users = userMapper.selectByExample(userExample);
-                    if (users.size() != 0) {
-                        HttpSession session = request.getSession();
-                        session.setAttribute("user", users.get(0));
-                        Long unreadCount = notificationService.unreadCount(users.get(0).getId());
-                        session.setAttribute("unreadCount", unreadCount);
+        @Autowired
+        private NotificationService notificationService;
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            Cookie[] cookies = request.getCookies();
+            if(cookies != null && cookies.length !=0)
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("token")) {
+                        String token = cookie.getValue();
+                        UserExample userExample = new UserExample();
+                        userExample.createCriteria().andTokenEqualTo(token);
+                        List<User> users = userMapper.selectByExample(userExample);
+                        if (users.size() != 0) {
+                            request.getSession().setAttribute("user", users.get(0));
+                            Long unreadCount = notificationService.unreadCount(users.get(0).getId());
+                            request.getSession().setAttribute("unreadCount", unreadCount);
+                        }
+                        break;
                     }
-                    break;
+                    //获取cookie中的token，并且和数据中的token对比，获取用户信息不为空时，再次设置session
                 }
-            }
-        return true;
+            return true;
+        }
+
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+        }
+
+        @Override
+        public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+        }
     }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-
-    }
-}
--- src/main/java/com/example/demo/interceptor/WebConfig.java
+++ src/main/java/com/example/demo/interceptor/WebConfig.java
@@ -3,15 +3,33 @@ package com.example.demo.interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
-//@EnableWebMvc
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private SessionInterceptor sessionInterceptor;
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
